fefadad1364c2a5a04769b691984e4188ab625c1
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2024-04-07 09:06:37 +0530

    responsiveness updated

--- src/components/BodySection.js
+++ src/components/BodySection.js
@@ -4,7 +4,7 @@ import "../App.css";
 
 const BodySection = () => {
   return (
-    <div className="mt-8 px-4 md:px-6">
+    <div className="mt-8 px-4">
       <div className="flex flex-col md:flex-row">
         <div className="flex-1 p-6">
           <h2 className="mb-4 text-white hedvig-letters-serif-uniquifier text-2xl md:text-4xl">
@@ -13,117 +13,117 @@ const BodySection = () => {
             story.
           </h2>
 
-          <p className="text-white text-sm mt-8">
-            MMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMM&nbsp;&nbsp;M
+          <p className="text-white mt-8 text-1">
+            MMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMM&nbsp;&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMMM&nbsp;M&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;M
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMMM&nbsp;M&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMM&nbsp;&nbsp;&nbsp;MMMMM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMMMM
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMM&nbsp;&nbsp;&nbsp;MMMMM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMMMM
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMMM&nbsp;M&nbsp;&nbsp;M
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;MMMMM&nbsp;M&nbsp;&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMM&nbsp;&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMMM&nbsp;&nbsp;M
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMM&nbsp;&nbsp;&nbsp;MMMMMM&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MMMM&nbsp;&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMMM&nbsp;&nbsp;MMMMM&nbsp;M
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMMM&nbsp;&nbsp;MMMMM&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMM&nbsp;&nbsp;&nbsp;MMMMMM
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMM&nbsp;&nbsp;&nbsp;MMMMMM
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
           </p>
 
-          <p className="text-white text-sm">
-            MMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
+          <p className="text-white text-1">
+            MMMMMMMMMMMMMMMMMMM&nbsp;&nbsp;MMMMMM
           </p>
 
-          <p className="text-white text-sm">MMMMMMMMMMMMMMMM</p>
+          <p className="text-white text-1">MMMMMMMMMMMMMMMMMMM</p>
 
-          <p className="text-white text-sm">MMMMMMMMMMMMMMM</p>
+          <p className="text-white text-1">MMMMMMMMMMMMMMMMMM</p>
 
-          <p className="text-white text-sm">MMMMMMMMMMMMMMMM</p>
-          <p className="text-white text-sm mt-8">
+          <p className="text-white text-1">MMMMMMMMMMMMMMMMMMM</p>
+          <p className="text-white text-3 mt-8">
             Read any article in our entire library across all your devices --
             with no paywalls, story, limits or ads
           </p>
         </div>
-        <div className="flex-1 p-6 md:border-l-2 md:pl-8 ">
+        <div className="flex-1 p-6 md:border-l-2 md:pl-8 flex-col md:flex-row ">
           <h2 className="text-2xl md:text-4xl mb-4 text-white hedvig-letters-serif-uniquifier">
             Support the voices you want
             <br />
             to hear more from.
           </h2>
 
-          <p className="text-white text-sm mt-8">
+          <p className="text-white text-1 mt-8">
             &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;M &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;
             &nbsp;&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;M&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;M &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp;M&nbsp; &nbsp;
             &nbsp;&nbsp;&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;
             &nbsp;M&nbsp;&nbsp; &nbsp; &nbsp;M&nbsp;&nbsp;
             &nbsp;&nbsp;M&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;M &nbsp;&nbsp;
             &nbsp;&nbsp;M&nbsp; &nbsp; &nbsp;&nbsp;&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;M&nbsp; &nbsp; M&nbsp;M&nbsp;
             M&nbsp; M&nbsp; &nbsp; M&nbsp; &nbsp; M&nbsp;&nbsp; M&nbsp;&nbsp;
             M&nbsp;&nbsp; M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;
-            &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
+            &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;&nbsp; &nbsp;
             &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
 
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp; &nbsp;
             &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp;
             &nbsp;&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
-          <p className="text-white text-sm">
+          <p className="text-white text-1">
             &nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;MM&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M&nbsp;M
           </p>
-          <p className="text-white text-sm">MMMMMMMMMMMMMMMMMMMMMMMMMMM</p>
-          <p className="text-white text-sm mt-8">
+          <p className="text-white text-1">MMMMMMMMMMMMMMMMMMMMMMMMMMM</p>
+          <p className="text-white text-3 mt-10">
             A portion of your membership will directly support the writers and
             thinkers you read the most
           </p>
--- src/components/ExploreSection.js
+++ src/components/ExploreSection.js
@@ -5,17 +5,14 @@ import "../App.css";
 const Explore = () => {
   const [isBelow900, setIsBelow900] = useState(false);
 
-  // Function to check window width on resize
   const handleResize = () => {
-    setIsBelow900(window.innerWidth < 900);
+    setIsBelow900(window.innerWidth > 900);
   };
 
-  // Add event listener on component mount
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
 
-    // Cleanup function to remove event listener
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -31,88 +28,176 @@ const Explore = () => {
         <button className="bg-black text-white font-sans py-1.5 px-12 rounded-full text-bold text-lg mt-8">
           Get unlimited access
         </button>
-        {/* Conditionally render based on screen size */}
+
         {!isBelow900 && (
           <div>
-            <p className="text-black text-3 explore-text font-sans">DISCOVER</p>
-            <p className="text-black text-3 explore-text-right font-sans">
-              EXPLORE
-            </p>
+            <div>
+              <p className="text-black text-1 explore-text font-sans">
+                DISCOVER
+              </p>
+              <p className="text-black text-1 explore-text-right font-sans">
+                EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-4 explore-text font-sans">
+                COVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-4 explore-text-right font-sans">
+                EXPLORE EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-8 explore-text font-sans">
+                R DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-8 explore-text-right font-sans">
+                EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-12 explore-text font-sans">
+                SCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-12 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-16 explore-text font-sans">
+                VER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-16 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-20 explore-text font-sans">
+                DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-20 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-24 explore-text font-sans">
+                COVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-24 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXLPOR
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-28 explore-text font-sans">
+                ER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-28 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPL
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-32 explore-text font-sans">
+                SCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-32 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOREE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-1 mt-36 explore-text font-sans">
+                VER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-1 mt-36 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOR
+              </p>
+            </div>
           </div>
         )}
-        {/* End of conditional rendering */}
-        <div>
-          <p className="text-black text-3 mt-4 explore-text font-sans">
-            COVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-4 explore-text-right font-sans">
-            EXPLORE EXPLORE
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-8 explore-text font-sans">
-            R DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-8 explore-text-right font-sans">
-            EXPLORE EXPLORE EX
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-12 explore-text font-sans">
-            SCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-12 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-16 explore-text font-sans">
-            VER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-16 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLO
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-20 explore-text font-sans">
-            DISCOVER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-20 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLORE EX
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-24 explore-text font-sans">
-            COVER DISCOVER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-24 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLORE EXLPOR
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-28 explore-text font-sans">
-            ER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-28 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPL
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-32 explore-text font-sans">
-            SCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-32 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOREE
-          </p>
-        </div>
-        <div>
-          <p className="text-black text-3 mt-36 explore-text font-sans">
-            VER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
-          </p>
-          <p className="text-black text-3 mt-36 explore-text-right font-sans">
-            EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOR
-          </p>
-        </div>
+
+        {isBelow900 && (
+          <div>
+            <div>
+              <p className="text-black text-3 explore-text font-sans">
+                DISCOVER
+              </p>
+              <p className="text-black text-3 explore-text-right font-sans">
+                EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-4 explore-text font-sans">
+                COVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-4 explore-text-right font-sans">
+                EXPLORE EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-8 explore-text font-sans">
+                R DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-8 explore-text-right font-sans">
+                EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-12 explore-text font-sans">
+                SCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-12 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-16 explore-text font-sans">
+                VER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-16 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-20 explore-text font-sans">
+                DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-20 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EX
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-24 explore-text font-sans">
+                COVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-24 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXLPOR
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-28 explore-text font-sans">
+                ER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-28 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPL
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-32 explore-text font-sans">
+                SCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-32 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOREE
+              </p>
+            </div>
+            <div>
+              <p className="text-black text-3 mt-36 explore-text font-sans">
+                VER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER DISCOVER
+              </p>
+              <p className="text-black text-3 mt-36 explore-text-right font-sans">
+                EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLORE EXPLOR
+              </p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
